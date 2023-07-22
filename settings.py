5202049ad5c369417668c3b2feeae5e4e04a460e
cukadarMert <cukadar.mertkaan@gmail.com>
2023-07-22 16:47:52 +0300

    DATABASE_CONF CLASS added

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+#EMAIL_HOST CLASS  
+
 class EMAIL_HOST:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
@@ -5,5 +7,16 @@ class EMAIL_HOST:
     EMAIL_HOST_PASSWORD = '<host password>'
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
+
+#DATABASE CLASS
+
+class DATABASE_CONF:
+    DATABASES = {"default": {"NAME": "db_name",
+                            "USER": "db_user",
+                            "PASSWORD": "db_password",
+                            "HOST": "db_host",
+                            "PORT": "db_port"}} 
+
+#LOG CLASS
 
 
