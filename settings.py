5333846ad8ffd6fc4555c40f972fda377d8ac5aa
cukadarMert <cukadar.mertkaan@gmail.com>
2023-07-22 16:21:48 +0300

    EMAIL HOST CLASS added

---
+++ settings.py
@@ -0,0 +1,9 @@
+class EMAIL_HOST:
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = '<host user>'
+    EMAIL_HOST_PASSWORD = '<host password>'
+    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = False
+
+
