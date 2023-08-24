da32ffa5139d8a53fcae3be75c893a260ab8958f
mertCukadar <cukadar.mertkaan@gmail.com>
2023-08-24 18:56:10 +0300

    variables and class methods defined

--- variables.py
+++ variables.py
@@ -1,1 +1,19 @@
 class VARIABLES:  
+    notified = False
+    stored_log_vars = []
+
+    @classmethod
+    def append_stored_log_vars(cls, log_var):
+        cls.stored_log_vars.append(log_var)
+
+    @classmethod
+    def clear_stored_log_vars(cls):
+        cls.stored_log_vars = []
+
+    @classmethod
+    def set_notified(cls):
+        cls.notified = not cls.notified
+
+    @classmethod
+    def get_notified(cls):
+        return cls.notified
