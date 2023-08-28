2e2f67724a83babda96a82684b6f20ad8a548a94
mertCukadar <cukadar.mertkaan@gmail.com>
2023-08-28 11:21:07 +0300

    main py updated respect to reviews

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     
     while True:
         # Create an instance for the QUERY class
-        query = """SELECT a.alarm_id, a.alarm_name, a.alarm_class, l.mailsent, l.currentstate , l.logdate
+        query = """SELECT a.alarm_id, a.alarm_name, a.alarm_class, l.currentstate , l.logdate
         FROM public.alarms a
         INNER JOIN logs.aalm_table l ON a.alarm_id = l.aalm_id
-        WHERE NOT l.mailsent AND l.currentstate = 1;
+        WHERE l.currentstate = 1;
         """
         query_vars = QUERY(query=query)
         
@@ -24,66 +24,57 @@ if __name__ == '__main__':
 
         
         try:
+            
+            
             collected_log_vars = DATABASE.collect_data(query_vars , conn=conn)
             
             
-            if len(collected_log_vars) > 0 :
-                for row in collected_log_vars:
-                    VARIABLES.append_stored_log_vars(row)  
-
-                # Send mail
-                mail_to = 'cukadar.mertkaan@gmail.com'
-                mail_subject = 'Soğutma Grubu Alarm Durumu'
-                mail_body = ''
-                mail_message = ''
-                        
-
-                check_mail = MailSend.sendmail(mail_to, mail_subject, mail_body, mail_message, data_list=VARIABLES.stored_log_vars , html_file_path='alarm_page.html')
+           
+            if len(collected_log_vars) > VARIABLES.get_len_stored_log_vars():
+                VARIABLES.clear_stored_log_vars()
                 
-                
-                if VARIABLES.get_notified() == True:
-                    VARIABLES.set_notified()
-
                 for row in collected_log_vars:
-                        if check_mail:
-                            # Update the logs table
-                            query = f"""UPDATE logs.aalm_table SET mailsent = true WHERE aalm_id = {row['alarm_id']}"""
-                            query_vars = QUERY(query=query)
-                            DATABASE.update_logs(query_vars , conn=conn)
+                    VARIABLES.append_stored_log_vars(row)
+
+                
+               
+                VARIABLES.set_notified_false()
+                VARIABLES.set_mailsent_false()
+
+           
+                
             
+                if VARIABLES.get_mail_sent() == False:
+                    
+                    # Send mail
+                    mail_to = 'cukadar.mertkaan@gmail.com'
+                    mail_subject = 'Soğutma Grubu Alarm Durumu'
+                    mail_body = ''
+                    mail_message = ''
+                            
 
+                    check_mail = MailSend.sendmail(mail_to, mail_subject, mail_body, mail_message, data_list=VARIABLES.stored_log_vars , html_file_path='alarm_page.html')
+                    VARIABLES.set_mail_sent()
+
+            
+            VARIABLES.set_len_stored_log_vars(len(collected_log_vars))
+
+            if len(collected_log_vars) == 0 and VARIABLES.get_notified() == False:
+                   
+                    
+                    data_list = []
+                    check_mail = MailSend.sendmail(mail_to, mail_subject, mail_body, mail_message, data_list=collected_log_vars , html_file_path='alarm_notify.html')
+                    
+                    VARIABLES.clear_stored_log_vars()
+                    VARIABLES.set_len_stored_log_vars(0)
+                    VARIABLES.set_notified_true()
+                    VARIABLES.set_mailsent_false()   
+                    
+        except:
+            pass    
+
+                   
+        
           
 
 
-            query = """SELECT a.alarm_id, a.alarm_name, a.alarm_class, l.mailsent, l.currentstate , l.logdate
-            FROM public.alarms a
-            INNER JOIN logs.aalm_table l ON a.alarm_id = l.aalm_id
-            WHERE l.mailsent AND l.currentstate = 1;
-            """
-            query_vars = QUERY(query=query)
-            active_alarm_vars = DATABASE.collect_data(query_vars , conn=conn)
-
-            if len(active_alarm_vars) == 0 and VARIABLES.notified == False:
-                
-                
-                if VARIABLES.get_notified() == False:
-                    VARIABLES.set_notified()
-                    
-                data_list = []
-                check_mail = MailSend.sendmail(mail_to, mail_subject, mail_body, mail_message, data_list=collected_log_vars , html_file_path='alarm_notify.html')
-                
-                if len(collected_log_vars) == 0:
-                    VARIABLES.clear_stored_log_vars()
-                    query2 = """UPDATE logs.aalm_table
-                    SET mailsent = false
-                    WHERE mailsent = true;
-                    """
-                    query_vars2 = QUERY(query=query2)
-                    DATABASE.update_logs(query_vars2 , conn=conn)
-               
-
-               
-            conn.close()       
-            
-        except:
-            pass
