61396bc961ad16a355f64c628de16d0d8cdb4e00
mertCukadar <cukadar.mertkaan@gmail.com>
2023-08-28 11:23:22 +0300

    Emaıl host conver to dict

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 #EMAIL_HOST CLASS  
 
-class EMAIL_HOST:
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_PORT = 587
-    EMAIL_HOST_USER = '<host user>'
-    EMAIL_HOST_PASSWORD = '<host password>'
-    EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = False
-
+EMAIL_HOST ={
+    "EMAIL_HOST": 'smtp.gmail.com',
+    "EMAIL_PORT": 587,
+    "EMAIL_HOST_USER": '<host user>',
+    "EMAIL_HOST_PASSWORD": "<host password>",
+    "EMAIL_USE_TLS": False,
+    "EMAIL_USE_SSL": False,
+}
 #DATABASE CLASS
 
 class DATABASE_CONF:
@@ -19,4 +19,4 @@ class DATABASE_CONF:
 
 #LOG CLASS
 
-
+  
